d59f54628f9635881a3202b5cb7ebdb1306e5d4d
Juan Hapes <juan.hapes@janiscommerce.com>
2026-05-28 14:01:45 +0200

    [master] - Bump coverallsapp action to v2 and add tests for serverless-helper-hooks and sqs sendMessagesBatch default baseResults

---
+++ tests/serverless-helper-hooks.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('assert');
+
+const serverlessHelperHooks = require('../lib/serverless-helper-hooks');
+
+describe('serverless-helper-hooks', () => {
+
+	afterEach(() => {
+		delete process.env.JANIS_WEBHOOKS_QUEUE_URL;
+	});
+
+	it('Should return an empty array when JANIS_WEBHOOKS_QUEUE_URL is not set', () => {
+		assert.deepStrictEqual(serverlessHelperHooks(), []);
+	});
+
+	it('Should return envVars and iamStatement hooks when JANIS_WEBHOOKS_QUEUE_URL is set', () => {
+
+		process.env.JANIS_WEBHOOKS_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/123456789012/janis-webhooks-queue';
+
+		assert.deepStrictEqual(serverlessHelperHooks(), [
+			['envVars', {
+				JANIS_WEBHOOKS_QUEUE_URL: '${env:JANIS_WEBHOOKS_QUEUE_URL}'
+			}],
+			['iamStatement', {
+				action: 'sqs:SendMessage',
+				resource: 'arn:aws:sqs:us-east-1:123456789012:janis-webhooks-queue'
+			}]
+		]);
+	});
+});
